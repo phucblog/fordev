97f47bbde03deeb1127a7f8fbf8267d2fba39c28
phucblog <phucblog@gmail.com>
2019-11-28 17:27:44 +0700

    Updates

---
+++ precache-manifest.c403b4c6626ccf3303f83092451b1fb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b834445b0a4dc7cb71deac75606cfbd0",
+    "url": "/fordev/index.html"
+  },
+  {
+    "revision": "b254ae7459fb130228a4",
+    "url": "/fordev/static/css/main.0c3be0f3.chunk.css"
+  },
+  {
+    "revision": "eceb43533ddd6dae53f1",
+    "url": "/fordev/static/js/2.73bb3935.chunk.js"
+  },
+  {
+    "revision": "b254ae7459fb130228a4",
+    "url": "/fordev/static/js/main.f77dadc5.chunk.js"
+  },
+  {
+    "revision": "63dcd812ea0cb7b4f5de",
+    "url": "/fordev/static/js/runtime-main.8595f89c.js"
+  },
+  {
+    "revision": "90ca052160aaa4b6396e473dc3ca9ec3",
+    "url": "/fordev/static/media/js.90ca0521.md"
+  },
+  {
+    "revision": "2ec7e4f8e4bea3e11efd770c5289b657",
+    "url": "/fordev/static/media/sample.2ec7e4f8.md"
+  }
+]);
